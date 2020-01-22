b1a69ff5c0879a800806021bd122312d3ced44ba
Ruben Lindström <ruben.lindstrom@gmail.com>
2020-01-22 19:27:31 +0100

    Added shifting bg on hover

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
-const hero = () => {
+const Hero = () => {
+  const [activeBg, setActiveBg] = useState(0);
   return (
     <div className="hero">
       <div className="inner-wrapper">
@@ -8,13 +9,43 @@ const hero = () => {
           <p>123 Fake Street — Seattle, WA — 206-555-7890</p>
         </div>
         <div className="bottom">
-          <h2>Eat</h2>
-          <h2>Drink</h2>
-          <h2>Visit</h2>
+          <h2
+            id="headerEat"
+            onMouseEnter={() => setActiveBg(1)}
+            onMouseLeave={() => setActiveBg(0)}
+          >
+            Eat
+          </h2>
+          <h2
+            id="headerDrink"
+            onMouseEnter={() => setActiveBg(2)}
+            onMouseLeave={() => setActiveBg(0)}
+          >
+            Drink
+          </h2>
+          <h2
+            id="headerVisit"
+            onMouseEnter={() => setActiveBg(3)}
+            onMouseLeave={() => setActiveBg(0)}
+          >
+            Visit
+          </h2>
         </div>
       </div>
+      <div
+        className={`onHover-bg ${activeBg === 1 ? "active" : ""}`}
+        id="bgEat"
+      ></div>
+      <div
+        className={`onHover-bg ${activeBg === 2 ? "active" : ""}`}
+        id="bgDrink"
+      ></div>
+      <div
+        className={`onHover-bg ${activeBg === 3 ? "active" : ""}`}
+        id="bgVisit"
+      ></div>
     </div>
   );
 };
 
-export default hero;
+export default Hero;
