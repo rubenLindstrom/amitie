1b2a806fee648be2ec3d915b5976cc76d7b58b49
Ruben Lindström <ruben.lindstrom@gmail.com>
2020-01-23 08:12:49 +0100

    Added staff members

--- src/pages/story.js
+++ src/pages/story.js
@@ -1,5 +1,33 @@
 import React from "react";
 
+const staff = [
+  {
+    image: require("../images/chef1.jpg"),
+    name: "Jeff Bonstar",
+    title: "Head Chef"
+  },
+  {
+    image: require("../images/chef2.jpg"),
+    name: "Mia Chun",
+    title: "Assistant Chef"
+  },
+  {
+    image: require("../images/chef3.jpg"),
+    name: "Pierre Fidier",
+    title: "Customer Experience"
+  },
+  {
+    image: require("../images/chef4.jpg"),
+    name: "Paul Ramsey",
+    title: "Reservations and menu"
+  },
+  {
+    image: require("../images/chef5.jpg"),
+    name: "Bebb",
+    title: "Very temporary intern"
+  }
+];
+
 const story = () => {
   return (
     <div id="story">
@@ -30,6 +58,15 @@ const story = () => {
         </p>
         <h2 className="subtitle">The staff</h2>
       </div>
+      {staff.map(({ image, name, title }) => (
+        <div className="staffMember">
+          <div className="image-wrapper">
+            <img src={image} alt={`${name} portrait`} />
+          </div>
+          <p className="staffName">{name}</p>
+          <p className="staffTitle">{title}</p>
+        </div>
+      ))}
     </div>
   );
 };
